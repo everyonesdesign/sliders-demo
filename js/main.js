5c36f527aedf312c8df271af9be43c823f59649a
Trambitskiy Yura <tr.yura@yahoo.com>
2013-12-10 18:30:21 +0400

    added and configured nivo slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+//General
 
+$(document).ready(function() {
+   $(".effect-tag").click(function(e) {
+       e.preventDefault();
+       $(this).addClass("active").siblings().removeClass("active");
+       if ($(this).closest(".nivo-slider-container").length) {
+           $("#nivoSlider").children("img").attr("data-transition", $(this).text());
+       }
+   });
+});
+
+$(window).load(function() {
+    $(".slider-container").animate({opacity: 1}, 200);
+});
+
+//Nivo slider
+$(window).load(function() {
+    $('#nivoSlider').nivoSlider({
+        effect:"sliceDown,sliceDownLeft,sliceUp,sliceUpLeft,sliceUpDown,sliceUpDownLeft,fold,fade,slideInRight,slideInLeft",
+        slices:15,
+        boxCols:8,
+        boxRows:4,
+        animSpeed:500,
+        pauseTime:3000,
+        startSlide:0,
+        directionNav:true,
+        controlNav:true,
+        controlNavThumbs:false,
+        pauseOnHover:true,
+        manualAdvance:false
+    });
+});
